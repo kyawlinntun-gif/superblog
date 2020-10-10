2060dd246d827306e67eb25e8155f95de6e12ff7
Kyaw Linn Tun <kyawlinntun100@gmail.com>
2020-10-10 10:42:27 +0630

    Create database

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UserSeeder::class);
+        $this->call(UserTableSeeder::class);
+        $this->call(CategoryTableSeeder::class);
+        $this->call(CommentTableSeeder::class);
+        $this->call(PostTableSeeder::class);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+// PostController
+Route::get('/post', 'PostController@index')->name('post.index');
